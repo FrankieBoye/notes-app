c6a95e266143d8126fcbbbb59b1979c94312d563
FrankieBoye <frankie4@btinternet.com>
2019-05-13 16:29:04 +0100

    refactored using assert.js

--- spec/note_spec.js
+++ spec/note_spec.js
@@ -2,10 +2,12 @@
   function Text() {
     var note = new Note('text');
 
-    if (note.returnText() !== 'text') {
-      throw new Error("Not text");
-    }
+assert.isTrue(note.returnText() === 'text');
   };
 
 Text();
 })(this);
+
+// if (note.returnText() !== 'text') {
+//   throw new Error("Not text");
+// }
